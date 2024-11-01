121ff509b5fe7ab360c44556d41bb4482281c8b5
Roman <60399380+cd-roman@users.noreply.github.com>
2024-11-01 20:08:39 +0100

    Add navigation and meals overview screen; update dependencies

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,13 +3,24 @@ import CategoryGridTile from "../components/CategoryGridTile";
 
 import { CATEGORIES } from "../data/dummy-data";
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+        categoryTitle: itemData.item.title,
+      });
+    }
 
-function CategoriesScreen() {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
   return (
     <FlatList
       data={CATEGORIES}
