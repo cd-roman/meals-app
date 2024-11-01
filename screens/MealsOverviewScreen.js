2eda5e7f97dbeb36fc04a746e5c9a9f4e240e53e
Roman <60399380+cd-roman@users.noreply.github.com>
2024-11-01 21:06:46 +0100

    Add MealItem component and implement FlatList in MealsOverviewScreen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,26 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import MealItem from "../components/MealItem";
 
 import { MEALS } from "../data/dummy-data";
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter(
+    (mealItem) => mealItem.categoryIds.indexOf(catId) >= 0
+  );
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      ></FlatList>
     </View>
   );
 }
